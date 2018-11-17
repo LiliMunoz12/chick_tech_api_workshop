cc1c137feb9cf090fcc15198967095229da68680
LiliMunoz12 <44322965+LiliMunoz12@users.noreply.github.com>
2018-11-17 15:01:23 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -13,10 +13,15 @@ $(document).ready(function(){
     console.log("call-api button was pressed");
 
     // call the API
-    var url = ""; // add your API's URL here!
+    var url = "https://api.nasa.gov/planetary/apod?api_key=B8UNxTRKr7FDWLcVDCYgJTpbr1Lpg6FnkpqGT2qE&date=2002-03-26&hd=false"; // add your API's URL here!
     $.get(url, function(data, status){
         console.log("Data: " + data + "\nStatus: " + status);
         console.log(data);
+        $("img").attr("scr", data.url);
+        $("p").html(data.explanation);
+
+
+
         // do something cool with the data here!!!
     });
 
